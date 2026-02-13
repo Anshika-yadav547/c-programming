c23aca54e333c024f61b31a13c150b82b40e0a9d
Anshika-yadav547 <yishita692@gmail.com>
2026-02-13 06:19:50 +0000

    Net salary calculation

--- first.c
+++ first.c
@@ -1,15 +1,13 @@
 #include<stdio.h>
 int main() {
-    int year;
-    printf("Enter year: ");
-    scanf("%d",&year);
-    if (year % 4 == 0)
-    {
-        printf("Leap year is %d",year);
-    }
-    else
-    {
-        printf("Not a Leap year %d",year);
-    }
+    float basic_salary, HRA, TA, gross_salary, PT, net_salary;
+    printf("Enter basic salary");
+    scanf("%f",&basic_salary);
+    HRA = basic_salary * 0.10;
+    TA = basic_salary * 0.05;
+    gross_salary = basic_salary + HRA + TA;
+    PT = gross_salary * 0.02;
+    net_salary = gross_salary - PT;
+    printf("%f",net_salary);
     return 0;
 }
