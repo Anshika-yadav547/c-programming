54e22f2826d3ea263118aa35b5a7f33357d7a0f2
Anshika-yadav547 <yishita692@gmail.com>
2026-02-13 06:39:05 +0000

    Even or Odd

--- first.c
+++ first.c
@@ -1,22 +1,16 @@
-#include<stdio.h>
-int main() {
-    int score;
-    printf("Enter the student's score (0-100): ");
-    scanf("%d",&score);
-    if (score >= 90) {
-        printf("Grade: A\n");
-    } else if (score >= 80) {
-        printf("Grade: B\n");
-    } else if (score >= 70) {
-        printf("Grade: C\n");
-    } else if (score >= 60) {
-        printf("Grade: D\n");
-    } else if (score >= 50) {
-        printf("Grade: E\n");
-    } else if (score >= 40) {
-        printf("Grade: F\n");
-    } else {
-        printf("FAIL");
+    #include<stdio.h>
+    int main() {
+    
+    int num;
+    printf("Enter number: ");
+    scanf("%d",&num);
+    if (num % 2  == 0)
+    {
+        printf("Number is even %d",num);
+    }
+    else
+    {
+        printf("Number is odd %d",num);
     }
     return 0;
 }
