b0be1fac61f25028cde86da71e05b1ed8709c2b7
Anshika-yadav547 <yishita692@gmail.com>
2026-02-13 05:52:18 +0000

    Addition after taking input

--- first.c
+++ first.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
-int main(){
-    int a = 10;
-    int b = 5;
+int main() {
+    int a,b;
+    printf("Enter two numbers: ");
+    scanf("%d%d",&a,&b);
+    printf("Number is %d %d\n",a,b);
     int c = a+b;
-    printf("Addition of two is %d\n",c);
+    printf("Addition of two number is: %d\n",c);
     return 0;
 }
