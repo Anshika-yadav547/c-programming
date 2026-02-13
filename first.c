4e67c09a69d806ea51a20862f04287cdedc1ee44
Anshika-yadav547 <yishita692@gmail.com>
2026-02-13 06:07:34 +0000

    Asking information from user

--- first.c
+++ first.c
@@ -1,10 +1,20 @@
 #include<stdio.h>
 int main() {
-    int a,b;
-    printf("Enter two numbers: ");
-    scanf("%d%d",&a,&b);
-    printf("Number is %d %d\n",a,b);
-    int c = a+b;
-    printf("Addition of two number is: %d\n",c);
+    char Name[50];
+    int roll;
+    char College[50];
+    int division;
+    printf("Enter Name\n");
+    scanf("%s",Name);
+    printf("Enter Roll No\n");
+    scanf("%d",&roll);
+    printf("Enter College name\n");
+    scanf("%s",College);
+    printf("Enter division\n");
+    scanf("%d",&division);
+    printf("Name: %s\n",Name);
+    printf("roll NO: %d\n",roll);
+    printf("College Name: %s\n",College);
+    printf("Division: %d\n",division);
     return 0;
 }
