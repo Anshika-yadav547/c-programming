a54403fe6f440b5cfab6b958acf1577d80d81892
Anshika-yadav547 <yishita692@gmail.com>
2026-02-13 06:52:58 +0000

    Simple Calculator

--- first.c
+++ first.c
@@ -1,16 +1,31 @@
-    #include<stdio.h>
-    int main() {
-    
-    int num;
-    printf("Enter number: ");
-    scanf("%d",&num);
-    if (num % 2  == 0)
+#include<stdio.h>
+int main() {
+    int a,b,ch,E,F;
+    float c,d;
+    printf("Enter numbers: ");
+    scanf("%d%d",&a,&b);
+    printf("Select switch: ");
+    scanf("%d",&ch);
+    switch(ch)
     {
-        printf("Number is even %d",num);
+        case(1):
+        E=a+b;
+        printf("Addition of two number: %d",E);
+        break;
+        case(2):
+        F=a-b;
+        printf("Subtraction of two number: %d",F);
+        break;
+        case(3):
+        c=a*b;
+        printf("Multiplication of two number: %f",c);
+        break;
+        case(4):
+        d=a/b;
+        printf("Division of two number: %f",d);
+        break;
+        default:
+        printf("choice is incorrect");
     }
-    else
-    {
-        printf("Number is odd %d",num);
-    }
-    return 0;
+return 0;
 }
