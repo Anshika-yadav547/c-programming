ea49cf2019caea092f3a395824e2b0abca4c7cf7
Anshika-yadav547 <yishita692@gmail.com>
2026-02-14 06:22:08 +0000

    Addition of Matrix

--- first.c
+++ first.c
@@ -1,22 +1,44 @@
 #include<stdio.h>
 int main() {
-    int a[2][2],i,j;
-    printf("Enter elements:\n");
-    for(i=0;i<2;i++)
+    int rows,cols;
+    printf("Enter number of rows: ");
+    scanf("%d",&rows);
+    printf("Enter numbers of columns: ");
+    scanf("%d",&cols);
+    int A[rows][cols], B[rows][cols] ,result[rows][cols];
+    printf("\nEnter elements of first matrix:\n");
+    for(int i =0; i < rows; i++)
     {
-        for(j=0;j<2;j++)
+        for(int j =0; j<cols;j++)
         {
-            printf("Enter elements [%d][%d]: ",i,j);
-            scanf("%d",&a[i][j]);
+            printf("A[%d][%d]: ",i,j);
+            scanf("%d",&A[i][j]);
         }
     }
-    printf("2 x 2 matrix is:\n");
-    for(i=0;i<2;i++)
+    printf("\nEnter elements of second matrix:\n");
+    for(int i = 0; i < rows; i++)
     {
-        for(j=0;j<2;j++)
+        for(int j = 0; j < cols; j++)
         {
-            printf("%d",a[i][j]);
+            printf("B[%d][%d]: ",i,j);
+            scanf("%d",&B[i][j]);
         }
+    }
+    for(int i = 0;i < rows;i++)
+    {
+        for(int j = 0;j<cols;j++)
+        {
+            result[i][j] = A[i][j] + B[i][j];
+        }
+    }
+    printf("\nResult of matrix addition:\n");
+    for(int i = 0; i < rows;i++)
+    {
+        for(int j = 0; j < cols;j++)
+        {
+            printf("%d",result[i][j]);
+        }
+        printf("\n");
     }
 return 0;
 }
