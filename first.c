bf39db7d72742a1b1f41c98dab94cbe45befa0e3
Anshika-yadav547 <yishita692@gmail.com>
2026-02-14 05:55:18 +0000

    Multiplication Table

--- first.c
+++ first.c
@@ -1,22 +1,11 @@
 #include<stdio.h>
 int main() {
-    int i;
-    int a[3], b[3], c[3];
-    printf("Enter numbers of a:\n");
-    printf("Enter numbers of b:\n");
-    for(i=0;i<3;i++)
+    int x;
+    printf("Enter the value of x: ");
+    scanf("%d",&x);
+    for(int i=1;i<=10;i++)
     {
-    scanf("%d",&a[i]);
-    }
-    for(i=0;i<3;i++)
-    {
-    scanf("%d",&b[i]);
-    }
-    printf("Sum of array c:\n");
-    for(i=0;i<3;i++)
-    {
-    c[i]=a[i]+b[i];
-    printf("%d",c[i]);
+        printf("%d x %d = %d\n",x,i,x*i);
     }
 return 0;
 }
