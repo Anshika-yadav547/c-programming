b1467f08d6329846e42c4956360cc3ab8dd7aac2
Anshika-yadav547 <yishita692@gmail.com>
2026-02-21 12:26:21 +0000

    Addition by taking numbers

---
+++ Input-Addition.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2,sum;
+    printf("Enter numbers: ");
+    scanf("%d%d",&num1,&num2);
+    sum = num1 + num2;
+    printf("Addition of two numbers: %d",sum);
+    return 0;
+}
