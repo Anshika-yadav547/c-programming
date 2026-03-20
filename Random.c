337542bffe142ca8afe752da0403a49565a2ebbb
Anshika-yadav547 <yishita692@gmail.com>
2026-03-20 06:16:56 +0000

    Random

---
+++ Random.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int num;
+    num = rand();             //generate random number
+    printf("Random number is: %d\n",num);
+    return 0;
+}
