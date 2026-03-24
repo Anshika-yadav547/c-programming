a9183fa1487ae1458b65eee9facac8c189033300
Anshika-yadav547 <yishita692@gmail.com>
2026-03-24 18:31:25 +0000

    matrix

---
+++ Additionofmatrix.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+int main()
+{
+    int a[2][2],b[2][2],c[2][2];
+    int i,j,k,l;
+    printf("Enter the values in array a\n");
+    for (i=0;i<2;i++)
+    {
+        for(j=0;j<2;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    printf("The values in matrix a ");
+    for(i=0;i<2;i++)
+    {
+        for(j=0;j<2;j++)
+        {
+            printf("%d",a[i][j]);
+        }
+        printf("\n");
+    }
+    printf("Enter the value in matrix b\n");
+    for(i=0;i<2;i++)
+    {
+        for(j=0;j<2;j++)
+        {
+            scanf("%d",&b[i][j]);
+        }
+    }
+    printf("The values in matrix b ");
+    for(i=0;i<2;i++)
+    {
+        for(j=0;j<2;j++)
+        {
+            printf("%d",b[i][j]);
+        }
+        printf("\n");
+    }
+    for(i=0;i<2;i++)
+    {
+        for(j=0;j<2;j++)
+        {
+            c[i][j] = a[i][j] + b[i][j];
+        }
+    }
+    printf("The addition of matrix is: ");
+    for(i=0;i<2;i++)
+    {
+        for(j=0;j<2;j++)
+        {
+            printf("%d",c[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
