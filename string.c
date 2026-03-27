d09135b896f6c67ffe4acbbe4e4b88569bd3b1c7
Anshika-yadav547 <yishita692@gmail.com>
2026-03-27 07:03:19 +0000

    length of string

---
+++ string.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int length =0;
+    char str1[10];
+    printf("Enter string:\n");
+    scanf("%s",str1);
+    while(str1[length]!='\0')
+    {
+        length++;
+    }
+    printf("length of string:%d",length);
+    return 0;
+}
